6d36b33ce54e426797f6b80e9871e752a460751d
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-09-26 22:00:46 +0200

    Create new index file
    Now the page has new index(main) file, so I can customize it easily
    
    This material helped me a lot - https://youtu.be/wlu8q6Q3Hro?t=761

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
+
+const Index = () => (
+  <Layout>
+    <h1>Hey!</h1>
+    <p>Yeeyy at last I changed the content on this page!</p>
+  </Layout>
+)
+
+export default Index
