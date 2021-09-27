e5ddd07213d895541fb2afd66b42cbb13706596b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-09-27 15:04:13 +0200

    Update content on the main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,8 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 
 const Index = () => (
   <Layout>
-    <h1>Hey!</h1>
-    <p>Yeeyy at last I changed the content on this page!</p>
+    <h1>My tech-blog</h1>
+    <p>My name is Michał, I am still learning software designer. I would like to try meself at running a tech blog(I have heard running a tech blog is a great experience and can teach a lot), so I found <a href="https://github.com/LekoArts/gatsby-starter-minimal-blog">this starter</a>, I combined it all with netlify, and these are my fruits of labor.</p>
   </Layout>
 )
 
